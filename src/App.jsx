0a909db525ee908fb3e4ee5789374099f5b9e519
Baljeet Singh <baljeetsinghhira@gmail.com>
2025-06-07 13:37:47 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to My App</h1>
+      <p>This is a simple React application.</p>
+      <p>Feel free to explore and modify the code!</p>
+      <p>Enjoy coding!</p>
+      <p>Have a great day!</p>
+      <p>Happy coding!</p>
+      <p>Keep learning!</p>
+      <p>React is awesome!</p>
+      <p>Check out the documentation for more features.</p>
+      <p>Don't forget to save your work.</p>
+      <p>Remember to commit your changes.</p>
+      <p>Share your app with friends.</p>
+    </div>
+  );
+};
+export default App;
